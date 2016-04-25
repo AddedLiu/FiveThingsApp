e3ef2c4642065fae1d947937f9edcb970d0eab2f
liuchuang <pyliuchuang@vip.qq.com>
2016-04-25 15:18:12 +0000

    android

---
+++ LiuChuang/app/src/main/java/cn/studyjamscn/s1/sj23/liuchuang/TextSize.java
@@ -0,0 +1,14 @@
+package cn.studyjamscn.s1.sj23.liuchuang;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class TextSize extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_size);
+    }
+
+}
